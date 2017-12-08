dd45badef9607dceb8600c5b6e7dfcbe922c2cc2
konner86 <leotyutyunikov@gmail.com>
2017-12-08 09:07:25 +0100

    update json

--- script.js
+++ script.js
@@ -23,3 +23,31 @@ var yyyy = datecrt.getFullYear();
     mm='0'+mm;
       } 
 datecrt = yyyy+'/'+mm+'/'+dd;
+
+var rIndex;
+                table = document.getElementById("table");
+var cnt = 1;               
+var request;
+				if (window.XMLHttpRequest) {
+				 request = new XMLHttpRequest();
+				}else {
+				 request = new ActiveXObject( "Microsoft.XMLHTTP");
+				}
+				 request.open('GET','second.json');
+				 request.onreadystatechange = function(){
+				 if ((request.readyState===4) && (request.status===200)){
+				 var items = JSON.parse(request.responseText);
+				 var output = '<tr>';{
+				 for(var key in items){
+							 output += '<td>' + cnt + '</td>';
+							 output += '<td>' + items[key].Name + '</td>';
+							 output += '<td>' + items[key].Quentity + '</td>';
+							 output += '<td>' + items[key].Comment + '</td>';
+							 output += '<td>' + items[key].Date + '</td>'; 
+							 cnt= cnt+1;
+							 output += '</tr>';}
+					document.getElementById('table').innerHTML+=output;
+				       }
+				 }
+		}
+ request.send();
